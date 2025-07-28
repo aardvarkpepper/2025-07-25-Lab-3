babf8b80cbcf068bb91fd5a6b3d62db4d6a522f5
James Lee <aardvarkpepper@gmail.com>
2025-07-27 21:26:40 -0400

    Clean up some comments.

--- src/App.tsx
+++ src/App.tsx
@@ -7,52 +7,15 @@ import { sortByKeyValue, getIndexSortedArray, getIndex } from './utils/utils.ts'
 
 import { Dropdown } from './components/Dropdown/Dropdown';
 
-// export const Dropdown = (elementName: string, arrayOfOptions: string[], selected: string) => {
-//   return (
-//     <select name = {elementName}>
-//       {arrayOfOptions.map((element) => <option value={element}>{element}</option>)}
-//     </select>
-//   );
-// }
-
 import './App.css';
 
-// export interface Task {
-//   id: string;
-//   title: string;
-//   description: string;
-//   status: TaskStatus;
-//   priority: 'low' | 'medium' | 'high';
-//   dueDate: string;
-// }
-
-// export type TaskStatus = 'pending' | 'in-progress' | 'completed';
-
-// export interface TaskListProps {
-//   tasks: Task[];
-//   onStatusChange: (taskId: string, newStatus: TaskStatus) => void;
-//   onDelete: (taskId: string) => void;
-// }
 
 // note: date string input "2025-07-25" turns to date object with dateObject = new Date (dateString)
 
-//onStatusChange, onPriorityChange, onDelete
-
-// onStatusChange: (taskId: string, newStatus: TaskStatus) => void;
-// onDelete: (taskId: string) => void;
-
-// triggers on dropdown
-const handleStatusChange = (event: any, taskId: Task, newPriority: string) => {
-  console.log(`Event.target.value: ${event.target.value}, taskID: ${taskId} newPriority: ${newPriority}`);
-};
-// triggers on dropdown.  Really doesn't need to be separate, could pass in arguments.
-const handlePriorityChange = (event: any, newPriority: string) => {
-  console.log(`Event.target.value: ${event.target.value}, newPriority: ${newPriority}`);
-};
-
 /**
  * 
- * implement filtering and sorting
+ * implement filtering and sorting.
+ * sorting, setState of tasks, import the sort function
  */
 
 function App() {
@@ -61,13 +24,11 @@ function App() {
   const handleDropdownKeyChange = (taskId: string, keyValue: keyof Task, newValue: string) => {
     // just go through array until finding the ID.  O(n); too many things in assignment operating effectively making sort unreliable.  Really, the database should be always sorted, and only views change, but implementation adds steps so eh.
     // so status change changes key 'status', priority change changes key 'priority'.
-    // remember state changes are a bother.
     //console.log(`core hDKC triggered, taskId ${taskId}, key ${keyValue}, newValue ${newValue}`);
-    // setTasklist(prev => []);
     setTasklist(prev => 
       prev.map((task) => {
         if (task.id === taskId) {
-          // honestly, what is the point of Typescript if I'm just throwing in these 'any' all the time to get around Typescript?  There's got to be a better way to do this.
+          // honestly, what is the point of Typescript if I'm just throwing in these 'any' all the time to get around Typescript?  There's got to be a better way.
           (task[keyValue] as any) = newValue;
           return task;
         } else {
@@ -88,16 +49,9 @@ function App() {
     <>
       <div>Test</div>
       <br />
-      {/* <Dropdown id="generic" elementName="IAmAnElement" arrayOfOptions={["cheese","ham","sausage"]} selected="ham" onChange = {(event) => (handleSelectChange(event))}/> */}
       <TaskList tasks={tasklist} onStatusChange={handleDropdownKeyChange} onPriorityChange={handleDropdownKeyChange} onDelete={handleDelete} />
     </>
   )
 }
 
 export default App;
-
-// interface DropdownProps {
-//   elementName: string;
-//   arrayOfOptions: string[];
-//   selected: string;
-// }
---
+++ src/components/SortButton/SortButton.tsx
@@ -0,0 +1,5 @@
+import { sortByKeyValue } from "../../utils/utils";
+
+const SortButton = () => {
+  
+}
